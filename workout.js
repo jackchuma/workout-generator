f308344d8744fb07c66307649d8de1ec9234e083
John Chuma <johnchuma@Johns-MacBook-Air.local>
2021-02-12 13:53:34 -0500

    Initial exercise bank created

---
+++ workout.js
@@ -0,0 +1,9 @@
+const exerciseBank = {
+    squat: ['Barbell Back Squat', 'Barbell Front Squat', 'Barbell Zercher Squat'],
+    hinge: ['Conventional Deadlift', 'Sumo Deadlift', 'Barbell RDL'],
+    lunge: ['Barbell Split Squat', 'DB Goblet Split Squat', 'DB Split Squat'],
+    verticalPush: ['Standing Barbell OH Press', 'Standing DB OH Press', 'Standing DB SA OH Press'],
+    verticalPull: ['Pull Up', 'Chin Up', 'Lat Pulldown'],
+    horizontalPush: ['Barbell Bench Press', 'DB Bench Press', 'Barbell Incline Bench Press'],
+    horizontalPull: ['Prone Incline DB Row', 'Bent Over Barbell Row', 'Seated Cable Row']
+}
