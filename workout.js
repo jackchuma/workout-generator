17e9cda16b229ecb82ad8aebe8181bdfbe80aa03
John Chuma <johnchuma@Johns-Air.fios-router.home>
2021-02-14 11:57:07 -0500

    New exercises

--- workout.js
+++ workout.js
@@ -2,12 +2,12 @@ const exerciseBank = {
     squat: ['Barbell Back Squat', 'Barbell Front Squat', 'Barbell Zercher Squat', 'DB Drop Squat', 'DB Goblet Squat', 'DB Front Curled Squat', 'Barbell Front Curled Squat', 'Barbell SL Back Squat', 'Bottoms-up KB + Reg KB Squat', 'Sideways Trap Bar Kickstand Squat', 'SL Barbell Front Curled Squat', 'SL DB Front Curled Squat', 'Bottoms-up KB Squat', 'Barbell OH Squat', 'Hanging Band Barbell Back Squats'],
     antSquat: ['Barbell Front Squat', 'Barbell Zercher Squat', 'DB Goblet Squat', 'DB Front Curled Squat', 'Barbell Front Curled Squat', 'Bottoms-up KB + Reg KB Squat', 'SL Barbell Front Curled Squat', 'SL DB Front Curled Squat', 'Bottoms-up KB Squat'],
     stabilitySquat: ['Hanging Band Barbell Back Squats', 'Barbell SL Back Squat', 'Sideways Trap Bar Kickstand Squat', 'SL Barbell Front Curled Squat', 'SL DB Front Curled Squat'],
-    hinge: ['Conventional Deadlift', 'Sumo Deadlift', 'Barbell RDL', 'DB RDL', 'Barbell RDL + Shrug', 'Double Barbell SL RDL', 'DB SL RDL', 'KB SL RDL + switch hands', 'KB Bottoms-Up SL RDL', 'Goblet RDLs', 'Goblet SL RDLs', 'Zercher Good Mornings'],
-    coreHinge: ['SL RDLs', 'DB SL RDL + switch hands', 'Goblet RDLs', 'Goblet SL RDLs', 'Zercher Good Mornings', 'KB Bottoms-up SL RDL'],
-    stabilityHinge: [],
+    hinge: ['Conventional Deadlift', 'Sumo Deadlift', 'Barbell RDL', 'DB RDL', 'Barbell RDL + Shrug', 'Double Barbell SL RDL', 'DB SL RDL', 'KB SL RDL + switch hands', 'KB Bottoms-Up SL RDL', 'Goblet RDLs', 'Goblet SL RDLs', 'Zercher Good Mornings', 'Barbell SL Good Morning'],
+    coreHinge: ['Barbell SL RDL', 'DB SL RDL', 'KB SL RDL + switch hands', 'Goblet RDLs', 'Goblet SL RDLs', 'Zercher Good Mornings', 'KB Bottoms-up SL RDL', 'Barbell SL Good Morning'],
+    stabilityHinge: ['Barbell SL RDL', 'DB SL RDL', 'KB SL RDL + switch hands', 'Goblet SL RDL', 'KB Bottoms-up SL RDL', 'Barbell SL Good Morning'],
     lunge: ['Barbell Split Squat', 'DB Goblet Split Squat', 'DB Split Squat', 'Barbell Reverse Lunge w/ slider', 'DB Reverse Lunge w/ slider', 'DB Lateral Lunge w/ slider', 'Zercher Reverse Lunge w/ slider', 'DB Pinch Grip Bulgarian Split Squat', 'Front Racked Barbell Split Squats', 'Front Racked KB Split Squats', 'Zercher Split Squats', 'Barbell Front Curled Split Squats', 'DB Front Curled Split Squats'],
     antLunge: ['DB Goblet Split Squat', 'Zercher Reverse Lunge w/ slider', 'Front Racked Barbell Split Squats', 'Front Racked KB Split Squats', 'Zercher Split Squats', 'Barbell Front Curled Split Squats', 'DB Front Curled Split Squats'],
-    stabilityLunge: [],
+    stabilityLunge: ['w/ front foot on pad', 'w/ back foot on roller', 'w/ back foot on MB', 'w/ back foot on PB', 'w/ eyes closed'],
     verticalPush: ['Standing Barbell OH Press', 'Standing DB OH Press', 'Standing DB SA OH Press', 'Bottoms-up Split Stance OH Press', 'Bench Seated Barbell OH Press', 'Bench Seated DB OH Press', 'Bench Seated SA DB OH Press', 'Kneeling Barbell OH Press', 'Kneeling DB OH Press', 'Kneeling DB SA OH Press', 'Kneeling Bottoms-up OH Press', '1/2 Kneeling Barbell OH Press', '1/2 Kneeling DB OH Press', '1/2 Kneeling DB SA OH Press', '1/2 Kneeling Bottoms-up OH Press', 'Floor Seated Barbell OH Press', 'Floor Seated DB OH Press', 'Floor Seated DB SA OH Press', 'Floor Seated Bottoms-up OH Press', 'PB Seated Barbell OH Press', 'PB Seated DB OH Press', 'PB Seated DB SA OH Press', 'PB Seated Bottoms-up OH Press'],
     coreVerticalPush: ['Kneeling Barbell OH Press', 'Kneeling DB OH Press', 'Kneeling DB SA OH Press', 'Kneeling Bottoms-up OH Press', '1/2 Kneeling Barbell OH Press', '1/2 Kneeling DB OH Press', '1/2 Kneeling DB SA OH Press', '1/2 Kneeling Bottoms-up OH Press', 'Floor Seated Barbell OH Press', 'Floor Seated DB OH Press', 'Floor Seated DB SA OH Press', 'Floor Seated Bottoms-up OH Press', 'PB Seated Barbell OH Press', 'PB Seated DB OH Press', 'PB Seated DB SA OH Press', 'PB Seated Bottoms-up OH Press'],
     stabilityVerticalPush: [],
@@ -264,7 +264,8 @@ function gatherWorkout() {
         const ex2 = generateExercise(exerciseBank.stabilitySquat);
         const ex3 = generateExercise(exerciseBank.stabilityHorizontalPull);
         const ex4 = generateExercise(exerciseBank.stabilityHorizontalPush);
-        const ex5 = generateExercise(exerciseBank.stabilityLunge);
+        const ex5Start = generateExercise(exerciseBank.lunge);
+        const ex5End = generateExercise(exerciseBank.stabilityLunge);
         const ex6 = generateExercise(exerciseBank.stabilityHinge);
         const ex7 = generateExercise(exerciseBank.singleArmVerticalPull);
         const ex8 = generateExercise(exerciseBank.stabilityVerticalPush);
@@ -285,7 +286,7 @@ function gatherWorkout() {
         console.log(`A2: ${ex2} - 2 x ${ex2Reps}`);
         console.log(`B1: ${ex3} - 2 x ${ex3Reps}`);
         console.log(`B2: ${ex4} - 2 x ${ex4Reps}`);
-        console.log(`C1: ${ex5} - 2 x ${ex5Reps}`);
+        console.log(`C1: ${ex5Start} ${ex5End} - 2 x ${ex5Reps}`);
         console.log(`C2: ${ex6} - 2 x ${ex6Reps}`);
         console.log(`D1: ${ex7} - 2 x ${ex7Reps}`);
         console.log(`D2: ${ex8} - 2 x ${ex8Reps}`);
